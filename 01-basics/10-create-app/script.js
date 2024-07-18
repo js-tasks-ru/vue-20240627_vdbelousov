36775f412d738f0de890b8b0dd55f2de1e3a58cc
89hikari <vladislav0151@bk.ru>
2024-07-19 00:16:20 +0300

    Task create-app has solved

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,10 @@
 import { defineComponent, createApp } from 'vue/dist/vue.esm-bundler.js'
+
+const App = defineComponent({
+  name: 'App',
+  template: `
+    <p>Сегодня {{new Date().toLocaleDateString('en-EN', { dateStyle: 'long' })}}</p>
+  `,
+})
+
+createApp(App).mount('#app')
