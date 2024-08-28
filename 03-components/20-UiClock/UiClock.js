75c2ebc747a6d3bcfd0caa863749ac7d4721bb7a
89hikari <vladislav0151@bk.ru>
2024-08-28 20:14:58 +0300

    UiClock task solved

--- 03-components/20-UiClock/UiClock.js
+++ 03-components/20-UiClock/UiClock.js
@@ -1,9 +1,29 @@
-import { defineComponent } from 'vue'
+import { computed, defineComponent, onUnmounted, ref } from 'vue'
 
 export default defineComponent({
   name: 'UiClock',
 
-  setup() {},
+  setup() {
+    const currentTime = ref(new Date())
 
-  template: `<div class="clock">10:12:02</div>`,
+    const updateTime = () => (currentTime.value = new Date())
+
+    const updateInterval = setInterval(updateTime, 1000)
+
+    onUnmounted(() => {
+      clearInterval(updateInterval)
+    })
+
+    const formatter = new Intl.DateTimeFormat(navigator.language, {
+      timeStyle: 'medium',
+    })
+
+    const formattedDate = computed(() => formatter.format(currentTime.value))
+
+    return {
+      formattedDate,
+    }
+  },
+
+  template: `<div class="clock">{{ formattedDate }}</div>`,
 })
